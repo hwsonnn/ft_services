061ad67101390a67fd73109640cd7c6cfd841474
손희원 <sonheewon@sonhuiwon-ui-MacBookPro.local>
2021-03-29 20:05:30 +0900

    0329 Mon

--- srcs/phpmyadmin/srcs/run.sh
+++ srcs/phpmyadmin/srcs/run.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz
-tar -xvf /phpMyAdmin-5.0.2-all-languages.tar.gz
-mv /phpMyAdmin-5.0.2-all-languages /etc/phpmyadmin
-
-rm -rf /phpMyAdmin-5.0.2-all-languages.tar.gz
-rm -rf /var/cache/apk/*
-
 php-fpm7 -R
 telegraf & nginx -g "daemon off;"
